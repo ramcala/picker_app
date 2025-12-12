99dec1976e01259aeed48ca6d78d5edd45aee651
Ram Pratap <rpratap@calance.com>
2025-12-12 07:55:55 +0530

    8000 port

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=80,
+        port=8000,
         reload=DEBUG,
         log_level="debug" if DEBUG else "info"
     )
